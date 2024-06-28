5817af977081797743203e292db03fae10ec6d80
adudewithapc <axel.holm.mc@hotmail.com>
2024-06-28 22:27:24 +0200

    Added ability to kill enemies

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
+class_name Enemy
 
 const SPEED = 50
 
 var player = Player
 @onready var anim = $Animation
+@onready var health_bar = $HealthBar
 
 func _physics_process(delta):
 	if(player == null):
@@ -15,6 +17,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+func attack(damage):
+	health_bar.health -= damage
 
 func _on_died():
 	queue_free()
