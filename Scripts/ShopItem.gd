63e759209b1b06690abf70846aa09a08d82cb1d4
adudewithapc <axel.holm.mc@hotmail.com>
2024-06-29 16:27:28 +0200

    Fixed compilation error

---
+++ Scripts/ShopItem.gd
@@ -0,0 +1,21 @@
+extends Control
+class_name ShopItem
+
+signal attempt_buy(item : ShopItem)
+
+@export var item_name : String
+@export var item_cost : int = 3
+@export var item_icon : Texture2D
+@export var consumable : bool
+
+@onready var _name := $Container/Details/Name
+@onready var _icon := $Container/Details/Icon
+@onready var _cost := $Container/Details/Cost/Amount
+
+func _ready():
+	_name.text = item_name
+	_icon.texture = item_icon
+	_cost.text = str(item_cost)
+
+func _on_details_input(event):
+	attempt_buy.emit(self)
