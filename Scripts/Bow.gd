837e8da5da54643949919c70271aef0521798f54
adudewithapc <axel.holm.mc@hotmail.com>
2024-06-29 13:36:54 +0200

    Bow shoots arrows

--- Scripts/Bow.gd
+++ Scripts/Bow.gd
@@ -1,11 +1,22 @@
 extends Weapon
 
-@onready var arrow := preload("res://Scenes/Arrow.tscn")
-@onready var timer := $Timer
+var arrow := preload("res://Scenes/Arrow.tscn")
+@onready var timer := $ShootTimer
 
 var can_shoot := true
 
 func _on_attack_initiated():
 	if can_shoot:
 		timer.start()
-		get_tree().root.add_child(arrow.instantiate())
+		create_arrow()
+		can_shoot = false
+
+
+func _on_shoot_timer_timeout():
+	can_shoot = true
+
+func create_arrow():
+	var new_arrow := arrow.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
+	new_arrow.position = global_position
+	new_arrow.global_rotation_degrees = global_rotation_degrees + 45
+	get_tree().root.add_child(new_arrow)
